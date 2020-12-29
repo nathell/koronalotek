549fe029aa341bab44279f4ac945e307350eddfd
Daniel Janus <dj@danieljanus.pl>
2020-12-29 10:51:08 +0000

    Automatic updater seems to be working!

--- src/koronalotek/updater.clj
+++ src/koronalotek/updater.clj
@@ -2,10 +2,12 @@
   (:require [chime.core :as chime]
             [clojure.string :as string]
             [java-time :as jtime]
+            [koronalotek.state :as state]
+            [koronalotek.time :as time]
             [reaver]))
 
 (defn parse-doc [doc]
-  (let [as-of-str (some-> (reaver/select q ".global-stats > p") first .childNodes first str (string/replace "Dane pochodzą z Ministerstwa Zdrowia, aktualne na : " "") .trim)]
+  (let [as-of-str (some-> doc (reaver/select ".global-stats > p") first .childNodes first str (string/replace "Dane pochodzą z Ministerstwa Zdrowia, aktualne na : " "") .trim)]
     {:csv-url (some-> doc (reaver/select ".file-download") second (reaver/attr :href))
      :as-of (-> (jtime/local-date-time "dd.MM.yyyy HH:mm" as-of-str)
                 (.atZone (jtime/zone-id "Europe/Warsaw"))
@@ -28,8 +30,23 @@
       (println "Fetch failed:" (.getMessage e))
       nil)))
 
-(defn job [time]
-  (println "Called at:" time))
+(def state (atom {:as-of #inst "2020-12-28T09:00:00.000-00:00"
+                  :cases 3211}))
+
+(defn job [now]
+  (let [previous-state @state
+        previous-as-of (:as-of previous-state)
+        diff (.getSeconds (jtime/duration previous-as-of now))]
+    (if (< diff 86400)
+      (println "Skipping update, less than a day passed since previous results")
+      (when-let [{:keys [as-of cases] :as new-data} (get-data)]
+        (if (and (jtime/before? (jtime/instant previous-as-of) (jtime/instant as-of))
+                 (not= (time/format-date as-of) (:date @state/state)))
+          (do
+            (println "New data fetched:" new-data)
+            (reset! state new-data)
+            (state/new-data! as-of cases))
+          (println "Fetched stale data:" new-data))))))
 
 (defn start [{:keys [every]}]
   (chime/chime-at (chime/periodic-seq (jtime/instant) (apply jtime/duration every))
