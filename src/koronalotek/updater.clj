394eaca3d5c5392011c282e9e059963b327d2ae1
Daniel Janus <dj@danieljanus.pl>
2021-01-21 10:50:12 +0000

    Update updater.

--- src/koronalotek/updater.clj
+++ src/koronalotek/updater.clj
@@ -1,34 +1,21 @@
 (ns koronalotek.updater
-  (:require [chime.core :as chime]
+  (:require [cheshire.core :as json]
+            [chime.core :as chime]
             [clojure.string :as string]
             [java-time :as jtime]
             [koronalotek.state :as state]
             [koronalotek.time :as time]
             [reaver]))
 
-(defn parse-doc [doc]
-  (let [as-of-str (some-> doc (reaver/select ".global-stats > p") first .childNodes first str (string/replace "Dane pochodzą z Ministerstwa Zdrowia, aktualne na : " "") .trim)]
-    {:csv-url (some-> doc (reaver/select ".file-download") second (reaver/attr :href))
-     :as-of (-> (jtime/local-date-time "dd.MM.yyyy HH:mm" as-of-str)
-                (.atZone (jtime/zone-id "Europe/Warsaw"))
-                (jtime/to-java-date))}))
-
-(defn fetch-and-extract-csv [{:keys [csv-url] :as data}]
-  (let [csv (slurp csv-url)
-        rows (string/split csv #"\r\n")
-        fields (map #(string/split % #";") rows)]
-    (merge data
-           {:cases (some-> fields second second Long/parseLong)})))
+(def data-uri
+  "https://services9.arcgis.com/RykcEgwHWuMsJXPj/arcgis/rest/services/global_corona_actual_widok2/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true")
 
 (defn get-data []
   (try
-    (-> (slurp "https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2")
-        (reaver/parse)
-        (parse-doc)
-        (fetch-and-extract-csv))
-    (catch Exception e
-      (println "Fetch failed:" (.getMessage e))
-      nil)))
+    (let [data (json/parse-string (slurp data-uri) keyword)
+          attributes (-> data :features first :attributes)]
+      {:cases (:ZAKAZENIA_DZIENNE attributes),
+       :as-of (java.util.Date. (:Data attributes))})))
 
 (def state (atom {:as-of #inst "2020-12-28T09:00:00.000-00:00"
                   :cases 3211}))
